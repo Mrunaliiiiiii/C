6f13ee6b779ff09eacfa29ad1514c6f79468b032
Mrunaliiiiiii <mrunali.lavangare1@gmail.com>
2025-09-18 03:27:06 +0000

    control statement

---
+++ controlStatement_if_else.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+
+int main(){
+    int age = 2;
+
+    // Simple If Statement 
+    // if(age>18){
+    //     printf("You can vote\n");
+    // }
+
+    // If else statement 
+    // if(age>18){
+    //     printf("You can vote\n");
+    // }else{
+    //     printf("You cannot vote\n");
+
+    // }
+
+    // If else if Ladder 
+    int marks=9;
+
+//     if(marks>90){
+//         printf("A Grade");
+//     }else if(marks>80){
+//          printf("B Grade");
+//     }
+//     else if(marks>70){
+//          printf("C Grade");
+//     }
+//     else if(marks>60){
+//          printf("D Grade");
+//     }
+//     else if(marks>35){
+//          printf("E Grade");
+//     }
+//     else {
+//          printf("F Grade");
+//     }
+
+
+// Nested If Else 
+int weight = 5;
+// if(age>18){
+//      if(weight>50){
+//           printf("Allowed Blood Donation");
+//      }
+//      else{
+//           printf("Not Allowed For Blood Donation weight criteria");
+
+//      }
+// }else{
+//           printf("Not Allowed For Blood Donation Age criteria");
+
+// }
+}

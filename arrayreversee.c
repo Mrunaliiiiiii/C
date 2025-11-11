9d3ca71c3229adcdfd8c562c607ff43f519e67d3
Mrunaliiiiiii <mrunali.lavangare1@gmail.com>
2025-11-11 04:19:13 +0000

    palindrome.c

--- arrayreversee.c
+++ arrayreversee.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
+
 int main() {
-int num, reversed = 0;
-printf("Enter an integer: ");
-scanf("%d", &num);
-while (num != 0) {
-int digit = num % 10;
-reversed = reversed * 10 + digit;
-num /= 10;
-}
-printf("Reversed number: %d\n", reversed);
-return 0;
+    int num,orgnum, reversed = 0;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    orgnum=num;
+    while (num != 0) {
+        int digit = num % 10;
+        reversed = reversed * 10 + digit;
+        num /= 10;
+    }
+
+    if(orgnum==reversed)
+    printf("palindrome");
+    else
+    printf("not palindrome");
+    return 0;
 }
